a1e8188d2eed5d3b493e8ba44ff1774015b1f973
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-02-24 17:56:22 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Employee {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter Name:");
+		String name = sc.nextLine();
+
+		System.out.println("Enter Salary/Day:");
+		int salPerDay = sc.nextInt();
+
+		System.out.println("Attendance in Days of that Month:");
+		int attendance = sc.nextInt();
+
+		int salary = salPerDay * attendance;
+
+		if (salary >= 10000) {
+			System.out.println("Hi " + name + ", your designation is Team Leader");
+		} else if (8000 <= salary && salary < 10000) {
+			System.out.println("Hi " + name + ", your designation is Senior Developer");
+		} else if (6000 <= salary && salary < 8000) {
+			System.out.println("Hi " + name + ", your designation is Junior Developer");
+		} else if (4000 <= salary && salary < 6000) {
+			System.out.println("Hi " + name + ", your designation is Trainee");
+		} else {
+			System.out.println("Hi " + name + ", your designation is Clerk");
+		}
+
+	}
+
+}

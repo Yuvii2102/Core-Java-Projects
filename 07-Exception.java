19dbea44b5360d3fa3d8e1c79110726c7c8e698f
Yuvraj.R <yuvrajmysuru@gmail.com>
2026-02-28 11:13:35 +0530

    Update 07-Exception.java

--- 07-Exception.java
+++ 07-Exception.java
@@ -1,31 +1,36 @@
 class PositiveException extends Exception 
 {
-
-	PositiveException(String msg) 
-  {
-		System.out.println(msg);
-	}
-
+    PositiveException(String msg) 
+    {
+        super(msg);
+    }
 }
 
-public class ThrowPositiveException 
+public class Main 
 {
+    void disp(int i) throws PositiveException 
+    {
+        if (i < 0) 
+        {
+            throw new PositiveException("Number is negative");
+        } 
+        else 
+        {
+            System.out.println("Number is positive");
+        }
+    }
 
-	void disp(int i) throws PositiveException 
-  {
-		if (i < 0) {
-			throw new PositiveException("Number is negative ");
-		} else {
-			System.out.println("number is positive");
-		}
-	}
+    public static void main(String[] args) 
+    {
+        Main t1 = new Main();
 
-	public static void main(String[] args) throws PositiveException 
-  {
-
-		Test4 t1 = new Test4();
-
-		t1.disp(-6);
-	}
-
+        try 
+        {
+            t1.disp(-6);
+        } 
+        catch (PositiveException e) 
+        {
+            System.out.println(e.getMessage());
+        }
+    }
 }
